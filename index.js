abf2924a0a9873b19432940bbf8c1edcd3cc84a3
Rui Marinho <rpm@seegno.com>
2015-03-30 14:51:29 +0100

    Add option to throw error instead of setting body

--- index.js
+++ index.js
@@ -46,9 +46,13 @@ function ratelimit(opts) {
     var remaining = limit.remaining > 0 ? limit.remaining - 1 : 0;
 
     // header fields
-    this.set('X-RateLimit-Limit', limit.total);
-    this.set('X-RateLimit-Remaining', remaining);
-    this.set('X-RateLimit-Reset', limit.reset);
+    var headers = {
+      'X-RateLimit-Limit': limit.total,
+      'X-RateLimit-Remaining': remaining,
+      'X-RateLimit-Reset': limit.reset
+    };
+
+    this.set(headers);
 
     debug('remaining %s/%s %s', remaining, limit.total, id);
     if (limit.remaining) return yield* next;
@@ -56,7 +60,12 @@ function ratelimit(opts) {
     var delta = (limit.reset * 1000) - Date.now() | 0;
     var after = limit.reset - (Date.now() / 1000) | 0;
     this.set('Retry-After', after);
+
     this.status = 429;
     this.body = 'Rate limit exceeded, retry in ' + ms(delta, { long: true });
+
+    if (opts.throw) {
+      this.throw(this.status, this.body, { headers: headers });
+    }
   }
 }
